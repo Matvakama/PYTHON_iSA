214b7ce11a8f5c22cf773d7e75025b06d5fa88c4
Matvakama <mat1297@wp.pl>
2019-06-26 20:17:49 +0200

    Initial commit

---
+++ Day2/string_exercise.py
@@ -0,0 +1,10 @@
+message = "Nie wiesz nic, Jonie Snow."
+
+print(message, '\n')
+
+imie = "Mateusz"
+nazwisko = "Grunwald"
+
+powitanie = 'Witaj, ' + imie + 'u ' + nazwisko + 'zie!'  # komentarz
+print(powitanie)
+print(f'Witaj, {imie} {nazwisko}!')
