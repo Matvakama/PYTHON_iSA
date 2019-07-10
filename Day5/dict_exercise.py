4027dd69cbd536d0e7b73c8cffb45bd0ae35426f
Matvakama <mat1297@wp.pl>
2019-07-10 19:56:48 +0200

    Updated dict_exercise. Added file_exercise.

--- Day5/dict_exercise.py
+++ Day5/dict_exercise.py
@@ -4,6 +4,9 @@ for key, value in first_dict.items():
 
 print(f"\n")
 
-film_dict = {2012:"Avengers", 1977:"Gwiezdne Wojny IV: Nowa nadzieja", 1988:"Szklana pułapka", 1982:"Rambo: Pierwsza krew", 2013:"Pacific Rim", 1981:"Indiana Jones i poszukiwacze zaginionej Arki", 2003:"Gdzie jest Nemo?"}
+film_dict = {2012:["Avengers", "2012", "Niesamowity Spider-Man"], 1977:["Gwiezdne Wojny IV: Nowa nadzieja"], 1988:["Szklana pułapka"], 1982:["Rambo: Pierwsza krew"], 2013:["Pacific Rim", "Iron Man 3", "Thor: Mroczny świat"], 1981:["Indiana Jones i poszukiwacze zaginionej Arki"], 2003:["Gdzie jest Nemo?"]}
 for key, value in film_dict.items():
-    print(f"Tytuł filmu: {value}. Rok wydania: {key}")
+    # print(f"{key}\n\t{value}")
+    print(f"\n{key}")
+    for movie in value:
+        print(f"\n\t{movie}")
---
+++ Day6/file_exercise.py
@@ -0,0 +1,19 @@
+# lista filmów
+
+list_of_movies = ["Obcy 3\n", "Terminator\n", "Dawno temu w trawie\n"]
+
+# with open("MovieList.txt", "w") as file:
+#    file.writelines(list_of_movies)
+    # for movie in list_of_movies:
+    #    file.write(f"{movie}\n")
+
+
+
+film_dict = {2012:["Avengers", "2012", "Niesamowity Spider-Man"], 1977:["Gwiezdne Wojny IV: Nowa nadzieja"], 1988:["Szklana pułapka"], 1982:["Rambo: Pierwsza krew"], 2013:["Pacific Rim", "Iron Man 3", "Thor: Mroczny świat"], 1981:["Indiana Jones i poszukiwacze zaginionej Arki"], 2003:["Gdzie jest Nemo?"]}
+
+with open("Movie_Report.txt", "w") as file:
+    for key, value in film_dict.items():
+        # print(f"{key}\n\t{value}")
+        file.write(f"\n{key}")
+        for movie in value:
+            file.write(f"\n\t{movie}")
