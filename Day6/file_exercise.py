de9ca4518c1bb1b8d65a6f630f6b8d9ab9665de7
Matvakama <mat1297@wp.pl>
2019-07-15 14:29:29 +0200

    general update of day 6

--- Day6/file_exercise.py
+++ Day6/file_exercise.py
@@ -11,9 +11,12 @@ list_of_movies = ["Obcy 3\n", "Terminator\n", "Dawno temu w trawie\n"]
 
 film_dict = {2012:["Avengers", "2012", "Niesamowity Spider-Man"], 1977:["Gwiezdne Wojny IV: Nowa nadzieja"], 1988:["Szklana pułapka"], 1982:["Rambo: Pierwsza krew"], 2013:["Pacific Rim", "Iron Man 3", "Thor: Mroczny świat"], 1981:["Indiana Jones i poszukiwacze zaginionej Arki"], 2003:["Gdzie jest Nemo?"]}
 
-with open("Movie_Report.txt", "w") as file:
-    for key, value in film_dict.items():
-        # print(f"{key}\n\t{value}")
-        file.write(f"\n{key}")
-        for movie in value:
-            file.write(f"\n\t{movie}")
+# with open("Movie_Report.txt", "w") as file:
+#     for key, value in film_dict.items():
+#         # print(f"{key}\n\t{value}")
+#         file.write(f"\n{key}")
+#         for movie in value:
+#             file.write(f"\n\t{movie}")
+
+# with open("Movie_Report.txt", "r") as file:
+#    from collections import defaultdict
